a450fbcdb94a448aaa20cd3852ef67ba959ca7ca
MohamedElsayedM <muhammedelsayed760@gmail.com>
2025-09-30 23:06:15 +0300

    Delete Implementation

--- Company.Route.PL/Controllers/DepartmentController.cs
+++ Company.Route.PL/Controllers/DepartmentController.cs
@@ -68,6 +68,71 @@ namespace Company.Route.PL.Controllers
   
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Invalid Id");
+            }
+            else
+            {
+                var department = _departmentRepository.Get(id.Value);
+                if (department is null) return NotFound(new { StatusCode = 404, Message = $"Department with Id :{id} is not found ." });
+                var model = new UpdateDepartmentDto() { 
+                Name= department.Name,
+                Code= department.Code,
+                CreationAt= department.CreationAt,
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id, UpdateDepartmentDto model)
+        {
+            if (ModelState.IsValid) { 
+                var department = new Department() { 
+                Id=id,
+                Name= model.Name,
+                Code= model.Code,
+                CreationAt=model.CreationAt
+                };
+            var Count =_departmentRepository.Update(department);
+                if (Count >0) return RedirectToAction("Index");
+            }
+            return View (model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Invalid Id");
+            }
+            else
+            {
+                var department = _departmentRepository.Get(id.Value);
+                if (department is null) return NotFound(new { StatusCode = 404, Message = $"Department with Id :{id} is not found ." });
+               
+                return View(department);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete([FromRoute] int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                if (department.Id != id) return BadRequest();
+
+                var Count = _departmentRepository.Delete(department);
+                if (Count > 0) return RedirectToAction("Index");
+            }
+            return View(department);
+        }
 
     }
 }
---
+++ Company.Route.PL/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Route.PL.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is required")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "CreationAt is required")]
+        public DateTime CreationAt { get; set; }
+
+    }
+}
